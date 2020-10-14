332bd563ef46178d89689be89203fda6b10f822d
fcsouza <fcsouza18@hotmail.com>
2020-10-13 21:33:16 -0300

    include db migration

---
+++ backend/src/database/migrations/1602599281866-create_orphanages.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class createOrphanages1602599281866
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orphanages',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 7,
+            precision: 9,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 7,
+            precision: 9,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages');
+  }
+}
