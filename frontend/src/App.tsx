cdbfdd53757db3155cfa5bd6332b90f4a33f7415
fcsouza <fcsouza18@hotmail.com>
2020-10-15 21:38:35 -0300

    include toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import 'leaflet/dist/leaflet.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
 
@@ -8,6 +11,7 @@ const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
+      <ToastContainer />
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
