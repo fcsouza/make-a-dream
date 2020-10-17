68a0f5399cc7f3defca6ffa05a06be931398f8d3
fcsouza <fcsouza18@hotmail.com>
2020-10-17 20:40:13 -0300

    refactor routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,7 +2,9 @@ import { AnimatePresence } from 'framer-motion';
 import React from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 
+import CreateOrphanages from '../pages/CreateOrphanages';
 import Landing from '../pages/Landing';
+import Orphanages from '../pages/Orphanages';
 import OrphanagesMap from '../pages/OrphanagesMap';
 
 const Routes: React.FC = () => {
@@ -11,7 +13,9 @@ const Routes: React.FC = () => {
     <AnimatePresence exitBeforeEnter>
       <Switch location={location} key={location.key}>
         <Route path="/" exact component={Landing} />
-        <Route path="/orphanages" exact component={OrphanagesMap} />
+        <Route path="/orphanages/map" exact component={OrphanagesMap} />
+        <Route path="/orphanages/create" exact component={CreateOrphanages} />
+        <Route path="/orphanages/details/:id" exact component={Orphanages} />
       </Switch>
     </AnimatePresence>
   );
