05da62a5c6e2b11cf043e03701e3a7f31d69c769
fcsouza <fcsouza18@hotmail.com>
2020-10-18 19:31:57 -0300

    include header

---
+++ mobile/src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import { Container, HeaderText } from './styles';
+
+interface HeaderProps {
+  title: string;
+  closeButton?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ title, closeButton }) => {
+  const navigation = useNavigation();
+
+  const handleNavigationToLandingPage = () => {
+    navigation.navigate('Landing');
+  };
+
+  return (
+    <Container style={{ borderBottomWidth: 1 }}>
+      <BorderlessButton onPress={navigation.goBack}>
+        <Feather name="arrow-left" color="#15b6d6" size={24} />
+      </BorderlessButton>
+      <HeaderText>{title}</HeaderText>
+      {closeButton && (
+        <BorderlessButton onPress={handleNavigationToLandingPage}>
+          <Feather name="x" color="#ff669d" size={24} />
+        </BorderlessButton>
+      )}
+    </Container>
+  );
+};
+
+export default Header;
---
+++ mobile/src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 24px;
+  background-color: #f9fafc;
+  border-color: #dde3f0;
+  padding-top: 44px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderText = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  color: #8fa7b3;
+  font-size: 16px;
+`;
