79d4a6cb27eac0ccc1588ec3ca6f20d74e48b629
fcsouza <fcsouza18@hotmail.com>
2020-10-19 22:04:20 -0300

    include routes

---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Header from '../components/Header';
+
+import Landing from '../pages/Landing';
+import OrphanageDetails from '../pages/OrphanageDetails';
+import CreateOrphanage from '../pages/CreateOrphanage';
+import SelectMapPosition from '../pages/SelectMapPosition';
+
+const { Navigator, Screen } = createStackNavigator();
+
+const Routes: React.FC = () => {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: true,
+        cardStyle: { backgroundColor: '#f2f3f5' },
+      }}
+    >
+      <Screen
+        name="Landing"
+        component={Landing}
+        options={{
+          headerShown: false,
+          header: () => <Header title="Exemplo" />,
+        }}
+      />
+      <Screen
+        name="OrphanageDetails"
+        component={OrphanageDetails}
+        options={{
+          header: () => <Header title="Detalhes do orfanato" />,
+        }}
+      />
+      <Screen
+        name="CreateOrphanage"
+        component={CreateOrphanage}
+        options={{
+          header: () => <Header title="Informações do orfanato" closeButton />,
+        }}
+      />
+      <Screen
+        name="SelectMapPosition"
+        component={SelectMapPosition}
+        options={{
+          header: () => <Header title="Onde está o orfanato ?" />,
+        }}
+      />
+    </Navigator>
+  );
+};
+
+export default Routes;
