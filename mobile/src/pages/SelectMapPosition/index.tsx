1040495524d125e2cdda1f159bf4148f805e9be4
fcsouza <fcsouza18@hotmail.com>
2020-10-19 21:59:31 -0300

    include mapPosition

---
+++ mobile/src/pages/SelectMapPosition/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Dimensions } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import MapView, { MapEvent, Marker } from 'react-native-maps';
+
+import mapMarkerImg from '../../assets/images/Local.png';
+
+import { Container, NextButton, NextButtonText } from './styles';
+
+const SelectMapPosition: React.FC = () => {
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+  const navigation = useNavigation();
+
+  function handleNextStep() {
+    navigation.navigate('CreateOrphanage', { position });
+  }
+
+  function handleMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate);
+  }
+
+  return (
+    <Container>
+      <MapView
+        onPress={handleMapPosition}
+        initialRegion={{
+          latitude: -22.901567,
+          longitude: -43.179578,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }}
+      >
+        {position.latitude !== 0 && (
+          <Marker
+            icon={mapMarkerImg}
+            coordinate={{
+              latitude: position.latitude,
+              longitude: position.longitude,
+            }}
+          />
+        )}
+      </MapView>
+
+      {position.latitude !== 0 && (
+        <NextButton onPress={handleNextStep}>
+          <NextButtonText>Próximo</NextButtonText>
+        </NextButton>
+      )}
+    </Container>
+  );
+};
+
+export default SelectMapPosition;
